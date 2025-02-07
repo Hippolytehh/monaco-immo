7af822a33eab56283c7aae52863fb7ac1263ca70
hippolyteh <frdistermin32@gmail.com>
2025-02-07 21:58:04 +0100

    add timestamp

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import { JSDOM } from 'jsdom';
 import fs from 'fs';
 
-const BASE_URL = 'https://www.chambre-immobiliere-monaco.mc/'
+const BASE_URL = 'https://www.chambre-immobiliere-monaco.mc/';
+
+const year = new Date().getFullYear();        // Get full year (e.g., 2025)
+const month = String(new Date().getMonth() + 1).padStart(2, '0'); // Get month, padded with 0 if needed
+const day = String(new Date().getDate()).padStart(2, '0');
+const TIMESTAMP = `${year}${month}${day}`
 
 async function fetchAndCacheHTML(cacheFile, url) {
 
@@ -23,7 +28,7 @@ async function fetchAndCacheHTML(cacheFile, url) {
 
 async function parseHTML() {
 
-    const cacheFile = './cache/cache.html';
+    const cacheFile = `./cache/cache_${TIMESTAMP}.html`;
 
     const html = await fetchAndCacheHTML(cacheFile, BASE_URL);
 
@@ -46,9 +51,31 @@ urls.forEach(async (x) => {
 
     const url = `${BASE_URL.slice(0, -1)}${x}`;
 
-    const cacheFile = `./cache/${url.split('/').slice(-2)[0]}.html`;
+    const cacheFile = `./cache/${url.split('/').slice(-2)[0]}_${TIMESTAMP}.html`;
 
-    console.log(cacheFile);
+    // console.log(cacheFile);
 
     const html = await fetchAndCacheHTML(cacheFile, url)
+
+    const dom = new JSDOM(html);
+
+    const document = dom.window.document;
+
+    const arr = Array.from(document.querySelector(".results.grid").querySelectorAll("article"));
+
+    const details = arr.map(x => {
+        // console.log()
+        return {
+            dataId: parseFloat(x.getAttribute('data-id')),
+            mainImage: x.querySelector("div.head img").href,
+            shortDescription: x.querySelector("div.head img").alt,
+            url: x.querySelector("a").href,
+            location: {
+                neighborhood: x.querySelector("div.descr p.lieu") ? x.querySelector("div.descr p.lieu").innerHTML.split(' - <span>').slice(0, 1) : null,
+                residence: x.querySelector("div.descr p.lieu span") ? x.querySelector("div.descr p.lieu span").innerHTML : null,
+            },
+        }
+    })
+
+    fs.writeFileSync('data.json', JSON.stringify(details));
 })
